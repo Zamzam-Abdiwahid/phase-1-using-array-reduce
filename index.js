219c7449157485853c127c180c0297775253427f
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-04-14 17:04:20 +0300

    Learnng how the reduce() method works

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 9, 10]; 
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries); // Should log the sum of all the assembled batteries
+
